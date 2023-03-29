109f93f1e7db45a004ece6c49f1b7042fdaf586e
colemilne54 <colemilne54@gmail.com>
2023-03-29 09:33:57 -0600

    Error in action function fixed

--- src/routes/edit.jsx
+++ src/routes/edit.jsx
@@ -7,7 +7,7 @@ import { updateContact } from "../contacts";
 
 export async function action({ request, params }) {
   const formData = await request.formData();
-  const updates = Object.formEntries(formData);
+  const updates = Object.fromEntries(formData);
   await updateContact(params.contactId, updates);
   return redirect(`/contacts/${params.contactId}`);
 }
