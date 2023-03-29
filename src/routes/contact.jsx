14b4ddb8fc9e88c39f0f36a5dbef2c76401e5492
colemilne54 <colemilne54@gmail.com>
2023-03-29 15:57:19 -0600

    1 file modified

--- src/routes/contact.jsx
+++ src/routes/contact.jsx
@@ -3,7 +3,14 @@ import {
   useLoaderData,
   useFetcher,
 } from "react-router-dom";
-import { getContact } from "../contacts";
+import { getContact, updateContact } from "../contacts";
+
+export async function action({ request, params }) {
+  let formData = await request.formData();
+  return updateContact(params.contactId, {
+    favorite: formData.get("favorite") === "true",
+  });
+}
 
 export async function loader({ params }) {
   const contact = await getContact(params.contactId);
