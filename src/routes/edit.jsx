33f9f960d71af947472faed2ac7496f95ab8ef34
colemilne54 <colemilne54@gmail.com>
2023-03-29 10:42:45 -0600

    2 files modified

--- src/routes/edit.jsx
+++ src/routes/edit.jsx
@@ -7,7 +7,11 @@ import { updateContact } from "../contacts";
 
 export async function action({ request, params }) {
   const formData = await request.formData();
+  const firstName = formData.get("first");
+  const lastName = formData.get("last");
   const updates = Object.fromEntries(formData);
+  updates.first;
+  updates.last;
   await updateContact(params.contactId, updates);
   return redirect(`/contacts/${params.contactId}`);
 }
